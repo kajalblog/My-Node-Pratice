5422b6f6ce3baf0ed35211ab825187b5942d5924
kajal parchande <kajal.parchande@greentiger.in>
2022-06-18 18:29:55 +0530

    Routing in node js

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
 import http from "http";
+import fs from 'fs';
 const server=http.createServer((req,res)=>{
     // res.setHeader('content-type','text');
     res.writeHead(240,"okay",{'content-type':'text/html'});
-    res.end("Response");
+    // res.end("Response");
+    if(req.url==='/')
+    {
+        fs.readFile('./public/home.html',(err,data)=>{
+            if(err) throw err;
+            res.end(data);
+        })
+    }
+    else if(req.url==='/about')
+    {
+        fs.readFile('./public/about.html',(err,data)=>{
+            if(err) throw err;
+            res.end(data);
+        })
+    }
+    else{
+        res.end("<h1>404 page not found</h1>")
+    }
 
 });
 const PORT=process.env.PORT ||3100;
